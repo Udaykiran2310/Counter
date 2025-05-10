627d51d9e1fc2899010e32ed6f45644cfbde6754
gov <gudaykiran2310@gmail.com>
2025-05-10 17:35:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ScoreCard from './components/ScoreCard/ScoreCard';
+import ScoreIncrementButton from './components/ScoreIncrementButton/ScoreIncrementButton';
 
 function App() {
+  let [score, scoreFun] = useState(0);
+  function incrementCounter(value){
+    score = score+value;
+    scoreFun(score);
+
+      // counter = counter + 1;
+      // console.log("updated counter", counter);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScoreCard score={score} />
+      <ScoreIncrementButton incrementCounter={incrementCounter}/>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/ScoreIncrementButton/ScoreIncrementButton.js
@@ -0,0 +1,16 @@
+import './ScoreIncrementButton.css';
+function ScoreIncrementButton(props){
+    
+    const{incrementCounter} = props;
+    
+    return(
+        <div className='flex-center'>
+        <button className='score-button'  onClick={function(){incrementCounter(1);}}>+1</button>
+        <button className='score-button'  onClick={function(){incrementCounter(5);}}>+5</button>
+        <button className='score-button'  onClick={function(){incrementCounter(-5);}}>-5</button>
+        <button className='score-button'  onClick={function(){incrementCounter(10);}}>+10</button>
+      </div>
+    );
+}
+
+export default ScoreIncrementButton; 
