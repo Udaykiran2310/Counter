317baa33a14782830cb1abd6054fdb7628aca4d1
Udaykiran2310 <103480671+Udaykiran2310@users.noreply.github.com>
2025-05-10 18:38:21 +0530

    Update ScoreIncrementButton.js

--- src/components/ScoreIncrementButton/ScoreIncrementButton.js
+++ src/components/ScoreIncrementButton/ScoreIncrementButton.js
@@ -9,6 +9,7 @@ function ScoreIncrementButton(props){
         <button className='score-button'  onClick={function(){incrementCounter(5);}}>+5</button>
         <button className='score-button'  onClick={function(){incrementCounter(-5);}}>-5</button>
         <button className='score-button'  onClick={function(){incrementCounter(10);}}>+10</button>
+        <button className='score-button'  onClick={function(){incrementCounter(100);}}>+100</button>
       </div>
     );
 }
